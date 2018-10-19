96b4c20117270687c16abaae82565ae974ea1c78
Klanc <clank201@gmail.com>
2018-10-19 14:56:38 +0200

    Millores al script, i nova versio del .tex

--- texLivePreview.sh
+++ texLivePreview.sh
@@ -3,11 +3,11 @@ if [ "$#" -ne 2 ]; then
         echo "Usage: ./texLivePreview.sh input.tex refreshInterval (in seconds)"
         exit
 fi
-pdftex -interaction=batchmode $1
-        xdg-open *.pdf
+pdflatex -interaction=batchmode $1
+zathura *.pdf &
 while true
 do
-	pdftex -interaction=batchmode $1
-	pkill -HUP mupdf
+	pdflatex -interaction=batchmode $1
+	#pkill -HUP mupdf
 	sleep $2s
 done
