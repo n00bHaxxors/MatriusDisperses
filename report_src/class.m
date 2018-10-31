44f7ac00a4a19a206ffa4efa42b3c32903773d23
sipwarriper <sipwarriper@gmail.com>
2018-10-31 16:32:24 +0100

    addRow al informe

---
+++ report_src/class.m
@@ -0,0 +1,23 @@
+%===============CSRSparseMatrix=================================================
+% Classe que implementa el mètode d'emmagatzematge per files sobre matrius 
+%%% disperses
+%
+% El constructor rep una matriu i hi aplica el metode, tornant un objecte
+%%% de tipus CSRSparseMatrix.
+%
+classdef CSRSparseMatrix
+  properties
+    Matrix 
+    %%% La matriu (estructura de quatre elements:
+    %%%                  n: Matrix.nColumns, 
+    %%%                  valors: Matrix.values, 
+    %%%                  columnes: Matrix.columns, 
+    %%%                  inici files: Matrix.beginningRow)
+  end
+
+  methods
+  %
+  %%% MÈTODES
+  %  
+  end
+end
