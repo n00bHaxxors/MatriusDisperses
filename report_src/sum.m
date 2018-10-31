074810c9de749d96a6c3c9440bc0c90e4eade1d0
Marc Cane <marc00077@hotmail.com>
2018-10-31 17:09:17 +0100

    sum.m

---
+++ report_src/sum.m
@@ -0,0 +1,19 @@
+%============sum=========================================================
+%
+%%% Donats obj i una altre matriu dispersa de la mateixa mida;
+%%%% retorna la suma de les dues matrius
+%
+function res = sum(obj, B)
+  nRowsA = length(obj.Matrix.beginningRow)-1;
+  nRowsB = length(B.Matrix.beginningRow)-1;
+  
+  assert(obj.Matrix.nColumns == B.Matrix.nColumns && nRowsA == nRowsB)
+  
+  res = CSRSparseMatrix([]);
+  res.Matrix.nColumns = obj.Matrix.nColumns;      
+  for i=1:nRowsB
+    rowA = obj.getRow(i);
+    rowB = B.getRow(i);
+    res = res.addRow(rowA+rowB);
+  end
+end
