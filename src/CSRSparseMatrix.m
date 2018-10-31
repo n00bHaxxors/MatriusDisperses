ad50820944158179c2350a9e8d4a1888983c36d8
Marc Cane <marc00077@hotmail.com>
2018-10-31 17:07:16 +0100

    implementada suma

--- src/CSRSparseMatrix.m
+++ src/CSRSparseMatrix.m
@@ -53,7 +53,7 @@ classdef CSRSparseMatrix
       end
     end
     
-    %============getElem========================================================
+    %============getElem=========================================================
     %
     %%% Donats obj, sent el objecte actual i (x,y) les coordenades del element 
     %%%% que volem obtenir;
@@ -64,7 +64,7 @@ classdef CSRSparseMatrix
       elem = row(y);
     end
     
-    %============getColumn======================================================
+    %============getColumn=========================================================
     %
     %%% Donats obj, sent el objecte actual, i y la columna que volem obtenir;
     %%%% retorna la columna y de la matriu obj
@@ -84,25 +84,25 @@ classdef CSRSparseMatrix
         end
       end
     end
-        
-    %============addRow=========================================================
+    
+    %============sum=========================================================
     %
-    %%% Donats obj, sent el objecte actual, i row la fila que volem afegir;
-    %%%% retorna obj amb la fila nova afegida
+    %%% Donats obj, sent el objecte actual, i y la columna que volem obtenir;
+    %%%% retorna la columna y de la matriu obj
     %
-    function obj = addRow(obj, row)
-      i=size(obj.Matrix.beginningRow,2)
-      nonZero=obj.Matrix.beginningRow(i)
-      nonZeroThisRow=0;
-      [_,n] = size(row);
-      for j = 1:n
-        if(row(j) ~= 0)
-          obj.Matrix.values = [obj.Matrix.values, row(j)]
-          obj.Matrix.columns = [obj.Matrix.columns, j]
-          nonZeroThisRow=nonZeroThisRow+1
-        end
-      end
-      obj.Matrix.beginningRow = [obj.Matrix.beginningRow, nonZero+nonZeroThisRow];
+    function res = sum(obj, B)
+      nRowsA = length(obj.Matrix.beginningRow)-1;
+      nRowsB = length(B.Matrix.beginningRow)-1;
+      
+      assert(obj.Matrix.nColumns == B.Matrix.nColumns && nRowsA == nRowsB)
+      
+      res = CSRSparseMatrix([]);
+      res.Matrix.nColumns = obj.n;
+      res.Matrix.values = [];
+      res.Matrix.columns = [];
+      res.Matrix.beginningRow = [1];
+      
+      for i=1:nRowsB
     end
     
   end
