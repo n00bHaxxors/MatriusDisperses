33ea5a9659d0f1c8d91b594a00ccd62ee0ca201a
sipwarriper <sipwarriper@gmail.com>
2018-11-05 22:45:32 +0100

    slides fins a operacions (incloses)

---
+++ report_src/getColumn.m
@@ -0,0 +1,20 @@
+%============getColumn=========================================================
+%
+%%% Donats obj, sent el objecte actual, i y la columna que volem obtenir;
+%%%% retorna la columna y de la matriu obj
+%
+function column = getColumn(obj, y)
+    column = 0;
+    m=size(obj.Matrix.beginningRow,2)-1;
+    for j=1:m
+        column(j)=0;
+        for i=obj.Matrix.beginningRow(j):obj.Matrix.beginningRow(j+1)-1
+            if obj.Matrix.columns(i)==y 
+                column(j) = obj.Matrix.values(i);
+                break
+            elseif obj.Matrix.columns(i)> y 
+                break
+            end
+        end
+    end
+end
